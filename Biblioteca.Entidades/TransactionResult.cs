cd2601dca72e0d4b56504c91d656e6892e4704a2
francomassimino <30MA38425551@campus.economicas.uba.ar>
2020-11-05 20:42:51 -0300

    Agregando atributos y propiedades de TransactionResult

--- Biblioteca.Entidades/TransactionResult.cs
+++ Biblioteca.Entidades/TransactionResult.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Biblioteca.Entidades
 {
-    class TransactionResult
+    public class TransactionResult
     {
+        public bool IsOk { get; set; }
+        public int Id { get; set; }
+        public string Error { get; set; }
+
     }
 }
