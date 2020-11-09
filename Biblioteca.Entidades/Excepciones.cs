e609f3939cdb6bc718b9601ff4971ced17d6930f
Augustojf98 <57848420+Augustojf98@users.noreply.github.com>
2020-11-09 10:50:39 -0300

    Create Excepciones.cs
    Se crean excepciones

---
+++ Biblioteca.Entidades/Excepciones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Entidades
+{
+    public class SinLibrosException : Exception
+    {
+        public SinLibrosException() : base("No hay libros cargados")
+        {
+
+        }
+    }
+
+    public class SinPrestamosException : Exception
+    {
+        public SinPrestamosException() : base("No hay préstamos cargados")
+        {
+
+        }
+    }
+
+    public class SinEjemplaresException : Exception
+    {
+        public SinEjemplaresException() : base("No hay ejemplares cargados")
+        {
+
+        }
+    }
+
+    public class SinClientesException : Exception
+    {
+        public SinClientesException() : base("No hay clientes cargados")
+        {
+
+        }
+    }
+}
